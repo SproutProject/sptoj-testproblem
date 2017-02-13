6ccf95aac2a2393e9e3e30d71602cf97b2ad47d9
PZ Read <netfirewall@gmail.com>
2017-02-13 17:04:57 +0000

    Add real problem

---
+++ 2/res/make/prbjudge.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+	int MaxHP,OrgHP,AddHP,FilHP;
+	MaxHP = 0; OrgHP = 0;
+	AddHP = 0; FilHP = 0;
+	while(scanf("%d%d%d",&OrgHP,&MaxHP,&AddHP)!=EOF)
+	{
+#include"main.cpp"
+
+		printf("%d\n",FilHP);
+	}
+	return 0;
+}
